c7591f390b89fad236122bf91b0440f85dc75d8e
Cédric Phung <cedric3011@outlook.fr>
2025-01-17 18:46:33 +0100

    add dummy data

--- flink/src/components/post/Post.jsx
+++ flink/src/components/post/Post.jsx
@@ -1,29 +1,34 @@
 import "./post.css";
+import { Users } from "../../dummyData"
 
-export default function Post() {
+export default function Post({post}) {
+
+const user = Users.filter(u=>u.id === 1)
   return (
     <div className="post">
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
-                    <span className="postUsername">John Doe</span>
-                    <span className="postDate">5 mins ago</span>
+                    <img className="postProfileImg" src={`assets/${Users.filter(u=>u.id === post.userId)[0].profilePicture}`} alt="" />
+                    <span className="postUsername">
+                        {Users.filter(u=>u.id === post.userId)[0].username}
+                    </span>
+                    <span className="postDate">{post.date}</span>
                 </div>
                 <div className="postTopRight">...</div>
             </div>
             <div className="postCenter">
-                <span className="postText">Hello ! It's my first post</span>
-                <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+                <span className="postText">{post?.desc}</span>
+                <img className="postImg" src={`assets/${post.photo}`} alt="" />
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
                     <img className="likeIcon" src="assets/heart.png" alt="" />
                     <img className="likeIcon" src="assets/like.png" alt="" />
-                    <span className="postLikeCounter">32 people like it</span>
+                    <span className="postLikeCounter">{post.like} people like it</span>
                 </div>
                 <div className="postBottomRight">
-                    <span className="postCommentText">9 comments</span>
+                    <span className="postCommentText">{post.comment} comments</span>
                 </div>
             </div>
         </div>
