984677a2102931ed14967a6aad527cb0ac19b8c1
Cédric Phung <cedric3011@outlook.fr>
2025-01-17 18:00:33 +0100

    modify homepage

--- flink/src/pages/home/Home.jsx
+++ flink/src/pages/home/Home.jsx
@@ -1,8 +1,18 @@
+import Sidebar from '../../components/sidebar/Sidebar';
 import Topbar from '../../components/topbar/Topbar';
+import Rightbar from '../../components/rightbar/Rightbar';
+import Feed from '../../components/feed/Feed';
 import './home.css';
 
 export default function Home() {
   return (
-    <div><Topbar/></div>
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
   )
 }
