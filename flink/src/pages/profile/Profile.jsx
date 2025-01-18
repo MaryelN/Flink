3f93045405d5b9da06415692d3309bcc820aadc7
MaryelN <maryel.navarromtz@gmail.com>
2025-01-18 12:46:49 +0100

    creation of the profile page

--- flink/src/pages/profile/Profile.jsx
+++ flink/src/pages/profile/Profile.jsx
@@ -11,12 +11,32 @@ export default function Profile() {
       <div className="profile">
         <Sidebar />
         <div className="profileRight">
-            <div className="profileRightTop"></div>
-            <div className="profileRightBottom"></div>
-            <Feed />
-            <Rightbar />
-        </div>
-      </div>
-    </>
+            <div className="profileRightTop">
+              <div className='profileCover'>
+                <img
+                className='profileCoverImg'
+                src="assets/person/3.jpeg"
+                alt=''
+                />
+                <img
+                className='profileUserImg'
+                src="assets/person/7.jpeg"
+                alt=''
+                />
+              <div className='profileInfo'>
+                <h4 className='profileInfoName'>Safak Kocaoglu</h4>
+                <span className='profileInfoDesc'>Hello friends</span>
+              </div>              
+              </div>
+            </div>
+            <div className="profileRightBottom">
+              <Feed />
+              <Rightbar profile/>
+            </div>
+          </div>
+          </div>
+        </>
+ 
   )
 }
+ 
