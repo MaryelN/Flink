c69613941494031a7822ba5c673494fa9b566f01
MaryelN <maryel.navarromtz@gmail.com>
2025-01-20 19:23:49 +0100

    Add of athentification & bcrypt

--- flink/api/routes/auth.js
+++ flink/api/routes/auth.js
@@ -1,15 +1,46 @@
 const router = require("express").Router();
 const User = require("../models/user")
+const bcrypt = require("bcrypt")
+
+
 //REGISTER
-router.get("/register", async (req,res)=> {
-  const user = await new User({
-    username:"john",
-    email: "john@gmail.com",
-    password: "12345"
-  }) 
+router.post("/register", async (req,res)=> {
+  
 
-  await user.save();
-  res.send("ok")
-})
+  try {
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    //create new user
 
-module.exports = router
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+
+  }catch(err){
+    res.status(500).json(err)
+  }
+});
+
+//LOGIN
+router.post("/login",async(req,res)=>{
+try {
+    const user = await User.findOne({email:req.body.email});
+    !user && res.status(400).json("user not found")
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("wrong password")
+
+    res.status(200).json(user)
+  } catch(err){
+    res.status(500).json(err)
+}
+});
+
+module.exports = router;
