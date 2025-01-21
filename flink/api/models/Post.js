a60b8f0e85560acb340c65f3d2c0d5d6516340fc
Cédric Phung <cedric3011@outlook.fr>
2025-01-21 15:08:28 +0100

    create post model

---
+++ flink/api/models/Post.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      max: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true } // Ajoute automatiquement createdAd et updatedAt
+);
+
+module.exports = mongoose.model("Posts", PostSchema);
