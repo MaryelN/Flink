4d3a82b64a2d66c52849bb53d0363347f1c9ec64
Cédric Phung <cedric3011@outlook.fr>
2025-01-21 15:12:35 +0100

    add middleware for post

--- flink/api/index.js
+++ flink/api/index.js
@@ -4,74 +4,69 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
-// const postRoute = require("./routes/posts");
-// const multer = require("multer");
-// const path = require("path");
-// const cors = require('cors');
- 
+
+const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
 dotenv.config();
- 
+
 // Connexion à MongoDB
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL);
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error('Could not connect to MongoDB:', err);
-        process.exit(1); // Terminer le processus en cas d'échec de la connexion
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Could not connect to MongoDB:", err);
+    process.exit(1); // Terminer le processus en cas d'échec de la connexion
+  }
 };
 connectDB();
 
-// //au lieu d'effectuer un get vers localhost8800::/images... afficher l'image
-// app.use("/images", express.static(path.join(__dirname, "public/images")))
+//au lieu d'effectuer un get vers localhost8800::/images... afficher l'image
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 // Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+// Middleware CORS global
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 
+app.use("/api/posts", postRoute);
 
-// // Middleware CORS global
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true,
-//   }));
- 
-// const userRoute = require("./routes/users");
-// const authRoute = require("./routes/auth");
- 
-// app.use("/api/users", userRoute);
-// app.use("/api/auth", authRoute);
- 
- 
-// app.use("/api/posts", postRoute);
- 
-// const storage = multer.diskStorage({
-//     destination:(req, file,cb) => {
-//         cb(null, "public/images")
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, req.body.name);
-//     }
-// })
- 
-// const upload = multer({storage: storage});
-// app.post("/api/upload", upload.single("file"), (req,res) => {
-//     try{
-//         return res.status(200).json("File uploaded successfully")
-//     }catch(err){
-//         console.log(err)
-//     }
-// })
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // Connecter sur le port local 8800
 app.listen(8800, () => {
-    console.log("backend server is running")
-})
+  console.log("backend server is running");
+});
